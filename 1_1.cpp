7401268b96421b7ad0df3a3f0a29483344f47796
blast3901 <118688043+blast3901@users.noreply.github.com>
2022-12-04 16:58:42 +0100

    Add files via upload

---
+++ 1_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string lines[2254];
+    ifstream input ("Day1/input.txt");
+    int elfCount = 1;
+    for (string& line : lines) {
+        getline(input,line);
+        if (line.empty()) ++elfCount;
+    }
+    input.close();
+
+    int *elfCals = new int[elfCount];
+    for (int i = 0; i < elfCount; i++) {
+        elfCals[i] = 0;
+    }
+
+    int j{};
+    for (string& line : lines) {
+        if(line.empty()) {
+            ++j;
+            continue;
+        }
+        elfCals[j] = elfCals[j] + stoi(line);
+    }
+
+    int max1{}, max2{}, max3{};
+    for (int i = 0; i < elfCount; i++){
+        if (elfCals[i] > max1) max1 = elfCals[i];
+        if (elfCals[i] > max2 && elfCals[i] < max1) max2 = elfCals[i];
+        if (elfCals[i] > max3 && elfCals[i] < max2) max3 = elfCals[i];
+    }
+    cout << max1 << endl;
+    return 0;
+}
