d591d4cbb404c07c4484adeab2e49a8b93732cc7
blast3901 <118688043+blast3901@users.noreply.github.com>
2022-12-06 13:52:15 +0100

    Add files via upload

---
+++ 6_1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace tuning{
+    namespace {
+        bool isMarker(const std::string& s, const int index, const int n) {
+            for (int i = index; i < index+n; i++) {
+                for (int j = index; j < index+n; j++) {
+                    if (i == j) continue;
+                    if (s[i] == s[j]) return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    std::string readFile(const std::string& path) {
+        std::string s;
+        std::ifstream input(path);
+        std::getline(input,s);
+        return s;
+    }
+
+    int findMarkerIndex(const std::string& s,const int prefixLength) {
+        for (int i = 0; i < s.length()-prefixLength; i++)
+            if (isMarker(s,i,prefixLength)) return i+prefixLength;
+        return -1;
+    }
+}
+
+int main() {
+    const int prefixLength = 4;
+    std::string input = tuning::readFile("Day6/input.txt");
+    std::cout << tuning::findMarkerIndex(input,prefixLength);
+    return 0;
+}
